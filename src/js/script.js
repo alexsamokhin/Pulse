4c6877ff628ebdff092f8469f2d6c32a42bfb167
Alex <samohin.flute@gmail.com>
2021-10-05 16:41:27 +0300

    adaptive till catalog

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
 	$('.carousel__inner').slick({
 		speed: 1200,
-/* 		adaptiveHeight: true, */
+		adaptiveHeight: true,
+		arrows: false,
 		prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron_left.png"></button>',
 		nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron_right.png"></button>',
 		responsive: [
 			{
-			breakpoint: 992,
+			breakpoint: 767,
 			settings: {
-				arrows: false,
-				dots: true
+				dots: false
 			  }
 			},
 			{
